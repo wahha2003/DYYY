e6516d4437f16431776c5a0f2668980f9a804776
wahha2003 <1316469326@qq.com>
2025-03-31 20:39:24 +0800

    移除冗余的 AWEIMRecallMessage 接口定义

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -597,6 +597,3 @@ typedef NS_ENUM(NSInteger, MediaType) {
 -(void)setRecallMessage:(BOOL)arg1;
 @property (nonatomic, strong) AWEIMRecallMessage *recallMessage;
 @end
-
-@interface AWEIMRecallMessage : NSObject
-@end
